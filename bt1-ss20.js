d829d752768dc8fe7d99a89601b63e90f2bed4f8
Phanh2910 <phanphuocanh97@gmail.com>
2025-03-05 06:44:11 +0700

    Update bt1-ss20.js

--- bt1-ss20.js
+++ bt1-ss20.js
@@ -12,10 +12,6 @@ function sums(N) {
     }
     return sum;
 }
-let n = +prompt("Nhập một số vào trong chương trình: ");
+let n = Number(prompt("Nhập một số nguyên dương: "));/* Yêu cầu người dùng nhập vào số nguyên dương  */
 let result = sums(n);
-if (result !== false) {
-    console.log(result);
-} else if (result === false) {
-    console.log(result);
-}
+console.log(result !== false ? `Tổng các số từ 1 đến ${n} là: ${result}` : "");/* Kiểm tra đúng sai về giá trị mà dev nhập vào */
